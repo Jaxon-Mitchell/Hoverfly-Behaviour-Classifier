851f5f045cdd18d8f4f6867980b45e5321c41b89
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-10-30 16:59:41 +1030

    Redid code to add inertia checking to prevent behaviours rapidly swapping during edge cases

--- classifyBehaviours.m
+++ classifyBehaviours.m
@@ -38,29 +38,39 @@ function analysisMatrix = classifyBehaviours(pathToDLCAnalysis, vidRes)
     count = 0;
 
     % Make a boolean of which legs are tucked
-    rTuckHind = false;
-    lTuckHind = false;
-    rTuckFront = false;
-    lTuckFront = false;
+    tucked.rHind = false;
+    tucked.lHind = false;
+    tucked.rFront = false;
+    tucked.lFront = false;
     
     for frame = 1:size(analysisMatrix, 1)
 
         if hindCalcs (frame, 1, 2) <= 20
-            rTuckHind = true;
+            tucked.rHind = true;
         end
         if hindCalcs(frame, 2, 2) <= 20
-            lTuckHind = true;
+            tucked.lHind = true;
         end
         if frontCalcs(frame, 1, 1) <= 7
-            rTuckFront = true;
+            tucked.rFront = true;
         end
         if frontCalcs(frame, 2, 1) <= 7
-            lTuckFront = true;
+            tucked.lFront = true;
         end
 
+        % This check is in place to deal with edge cases and prevent rapid
+        % switching between behaviours
         if currentBehaviour ~= 0
-            checkCurrentBehaviour(currentBehaviour, count, other)
+            [count, outcome] = checkCurrentBehaviour(currentBehaviour, count, tucked, WBA(frame, :), frontCalcs(frame, :, :), squeeze(hindCalcs(frame, :, :)));
+            if outcome == 1
+                analysisMatrix(frame, 2) = currentBehaviour;
+                continue
+            end
         end
+
+        % If it is no longer the same held behaviour, go through the
+        % flowchart to find the current behaviour.
+        count = 0;
         % Check if participant is flying
         if WBA(frame, 1) < 60 && WBA(frame, 2) < 60
             analysisMatrix(frame, 2) = 1;
@@ -68,23 +78,11 @@ function analysisMatrix = classifyBehaviours(pathToDLCAnalysis, vidRes)
             % If not, continue
             continue
         end
-        % Check if participant has all limbs out (Starfish)
-        if ~rTuckHind && ~lTuckHind && ~rTuckFront && ~lTuckFront
+        % Check if participant has all limbs out (Starfish) NEED TO REDEFINE THIS
+        if ~tucked.rHind && ~tucked.lHind && ~tucked.rFront && ~tucked.lFront
             analysisMatrix(frame, 2) = 6;
             currentBehaviour = 6;
-            count = 0; % Give this a different name
             continue
-        elseif currentBehaviour == 6
-            % We want to double check before transitioning out of starfish
-            if all([~rTuckHind, ~lTuckHind]) && any([~rTuckFront, ~lTuckFront])
-                analysisMatrix(frame, 2) = 6;
-                count = 0;
-                continue
-            elseif count <= 3
-                analysisMatrix(frame, 2) = 6;
-                count = count + 1;
-                continue
-            end
         end
         % Are the wings turning?
         if abs(WBA(frame, 1) - WBA(frame, 2)) > 15
@@ -92,9 +90,9 @@ function analysisMatrix = classifyBehaviours(pathToDLCAnalysis, vidRes)
             hingeKneeAngleDiff = abs(hindCalcs(frame, 1,3) - hindCalcs(frame, 2,3));
             leftRudderTest = hindCalcs(frame, 2,1) > 25 && hingeKneeAngleDiff > 15;
             rightRudderTest = hindCalcs(frame, 1,1) > 25 && hingeKneeAngleDiff > 15;
-            tuckTest = xor(rTuckHind, lTuckHind);
+            tuckTest = xor(tucked.rHind, tucked.lHind);
             % Check if participant is doing a turning starfish
-            if ~rTuckHind && ~lTuckHind && xor(~rTuckFront, ~lTuckFront)
+            if ~tucked.rHind && ~tucked.lHind && xor(~tucked.rFront, ~tucked.lFront)
                 analysisMatrix(frame, 2) = 7;
                 currentBehaviour = 7;
                 continue
@@ -111,7 +109,7 @@ function analysisMatrix = classifyBehaviours(pathToDLCAnalysis, vidRes)
             continue
         end
         % Check if participant is in superman pose
-        if ~rTuckHind && ~lTuckHind && rTuckFront && lTuckFront
+        if ~tucked.rHind && ~tucked.lHind && tucked.rFront && tucked.lFront
             analysisMatrix(frame, 2) = 5;
             currentBehaviour = 5;
             continue 
@@ -140,8 +138,63 @@ end
 % [rightVectorLength, rightRelativeAngle; ...
 %  leftVectorLength,  leftRelativeAngle]
 
-function check()
+% This function is designed to provide inertia to prevent rapid switching
+% between behaviours during edge cases
+function [count, outcome] = checkCurrentBehaviour(currentBehaviour, count, tucked, WBA, ~, hindCalcs) % Add back in frontCalcs where '~' later on
+    % 'outcome' refers whether to stay on the same behaviour (= 1) or to
+    % move onto the next behaviour (= 0)
+    outcome = 1;
+    switch currentBehaviour
+        case 1 % Not flying
+            if all([WBA(1) < 60, WBA(2) < 60])
+                count = 0;
+            else
+                count = count + 1;
+            end
+        case 2 % Flying Straight
+            if abs(WBA(1) - WBA(2)) < 15 && any([WBA(1) > 60, WBA(2) > 60])
+                count = 0;
+            elseif all([~tucked.rHind, ~tucked.lHind]) || any([tucked.rFront, tucked.lFront])
+                count = 3;
+            else
+                count = count + 1;
+            end
+        case 3 % Turning with no additional behaviour
+            if all([tucked.rHind, tucked.lHind, tucked.rFront, tucked.lFront]) && abs(WBA(1) - WBA(2)) > 15 && any([WBA(1) > 60, WBA(2) > 60])
+                count = 0;
+            else
+                count = count + 1;
+            end
+        case 4 % Ruddering
+            hingeKneeAngleDiff = abs(hindCalcs(1,3) - hindCalcs(2,3));
+            leftRudderTest = hindCalcs(2,1) > 25 && hingeKneeAngleDiff > 15;
+            rightRudderTest = hindCalcs(1,1) > 25 && hingeKneeAngleDiff > 15;
+            tuckTest = xor(tucked.rHind, tucked.lHind);
+            if any([~tucked.rFront, ~tucked.lFront])
+                count = 3;
+            elseif leftRudderTest || rightRudderTest || tuckTest
+                count = 0;
+            else
+                count = count + 1;
+            end
+        case 5 % Superman position
+            if all([~tucked.rHind, ~tucked.lHind]) && all([tucked.rFront, tucked.lFront])
+                count = 0;
+            else
+                count = count + 1;
+            end
+        case 6 % Starfish
+            if all([~tucked.rHind, ~tucked.lHind]) && any([~tucked.rFront, ~tucked.lFront])
+                count = 0;
+            else
+                count = count + 1;
+            end
 
+    end
+
+    if count >= 2
+        outcome = 0;
+    end
 end
 
 
